0380da4fee7b4a926ffdbb6b068f80c0a3f5d156
Adil Nawaz <adilnawaz256@gmail.com>
2023-07-14 17:32:47 +0530

    routingfix

--- src/components/IssueComponents.jsx
+++ src/components/IssueComponents.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import  "./css/IssueComponent.css"
+import { useParams } from 'react-router-dom'
 
 const IssueComponents = () => {
+  const {number} = useParams()
+
   return (
     <div
-    className='w-36 h-36 border-black flex justify-center'
-    >IssueComponents</div>
+    className='issue-container shadow-xl'
+    >
+<h3 className='font-bold text-[#FACA05] text-xl hover:text-black'>
+  <span className='bg-yellow-500 text-white font-semibold px-2 py-1 rounded mr-2'>
+    Issue
+  </span>
+  This is issue number {number}
+</h3>
+
+    </div>
   )
 }
 
